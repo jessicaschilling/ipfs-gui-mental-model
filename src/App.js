d115fcf9f8620c6a5f52ce3698842e489928263d
Jessica Schilling <jessica@protocol.ai>
2020-06-09 16:48:12 -0600

    Favicon etc; first pass left nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import ipfsLogo from './ipfs-logo.svg';
 import Header from './Header';
+import Nav from './Nav';
 import './style.css';
 import 'tachyons';
 import 'ipfs-css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="vh-100">
+      <Header />
+      <div className="flex h-100">
+        <Nav />
 
-    <Header />
+        <div className='w-60'>
 
-foo
+        What is this?
+        This IPFS GUI family mental model offers a framework for understanding how IPFS’ various GUI-based tools work together to provide cohesive, consistent enablers to using and building on IPFS for a variety of developer and non-developer stakeholder groups. 
+        Source documents
+        This model was created in Summer 2020 as part of an overarching effort to evaluate backlogs, calibrate on user needs, and reboot value-driving work on the IPFS GUI family. It draws heavily on core-team discussion/iteration (including the February 2020 IPFS Team Week), as well as the following foundational work:
+        • IPFS Information Ecosystem Audit: Baseline definitions and stack-ranks for IPFS’ stakeholders, workstreams (journeys), core goals, and channels
+        • Spring 2020 GUI Tools Use Survey: Open survey to the global IPFS community on which IPFS GUI tools they use, how, and why
+        • IPFS Companion Uninstall Feedback Survey: Optional “why” survey suggested upon a Companion user’s uninstalling the extension
+        • Fall 2019 Colorado IPFS User Testing Day: Open-ended user testing (within the framework of the docs beta launch) on desires, pain points, and goals
+        • Summer 2019 User/Goal Personae: Initial work on core user personae, validated by engaged IPFS collabs and expanded upon in the ecosystem audit 
+        How to use this model
+        This model is based on three primary axes: apps (or app types), repos, and stakeholders. Each item within one of the axes (for example, the ipfs-css repo) relates to one or more items elsewhere (for example, ipfs-css is instrumental in all six apps/app types, and is relevant to all stakeholder groups). To better understand how all these items work together to provide overall tooling for users exploring, using, or building on IPFS, simply explore:
+        • Apps & app types illustrate how the 17 repos in this model combine to create a unified tool set — highlighting frequently-used repos and unsung heroes • Repos illustrate how each project factors into the ecosystem audit’s 9 core stakeholder goals, the stakeholders for which a project is useful, and    our assumptions of user savvy for each project • Stakeholders consolidates the ecosystem audit’s 14 core stakeholder types into 7 types relevant to this work, and offers both a basic journey map and    use-by-time/expertise quadrant graph for each  As you explore the mental model, it’s also very useful to consider this work alongside other industry-wide longitudinal studies, such as the luence DWeb Developer Report from June 2020. 
+        Comments, questions, issues
+        This artifact was originally created by @jessicaschilling in June 2020 and is subject to ongoing modification as the apps, repos, and stakeholders referenced continue to evolve. For questions or comments, please feel free either to leave a comment in this Figma prototype itself, or (particularly if you have something specific and actionable in mind) open an issue in the ipfs/ipfs-gui repo.
+        </div>
 
+      </div>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './style.css';
+import 'tachyons';
+import 'ipfs-css';
+
+const Nav = () => {
+  return (
+    <div className='bg-snow pa3 w-15'>
+      <div className='f4 fw5 montserrat charcoal ttu mb1'>Welcome</div>
+      <div className='link'>About this model</div>
+
+      <div className='f4 fw5 montserrat charcoal ttu mt4 mb1'>Tools &amp; tool types</div>
+      <div className='link'>IPFS Companion</div>
+      <div className='link mt1'>Web UI</div>
+      <div className='link mt1'>IPFS Desktop</div>
+      <div className='link mt1'>Misc tools</div>
+      <div className='link mt1'>Misc info/download sources</div>
+      <div className='link mt1'>Included by third parties</div>
+
+      <div className='f4 fw5 montserrat charcoal ttu mt4 mb1'>Stakeholders</div>
+      <div className='link'>Devs: dedicated/pro/collab</div>
+      <div className='link mt1'>Devs: hobbyist/hacker</div>
+      <div className='link mt1'>Business users: eng mgrs</div>
+      <div className='link mt1'>Crypto enthusiasts: devs</div>
+      <div className='link mt1'>Crypto enthusiasts: non-devs</div>
+      <div className='link mt1'>Dweb-curious: relevant area</div>
+      <div className='link mt1'>Dweb-curious: “civilians”</div>
+
+      <div className='f4 fw5 montserrat charcoal ttu mt4'>Repos</div>
+      <div className='f5 fw4 montserrat charcoal ttu mt2'>ipfs org</div>
+      <div className='link'>dir-index-html</div>
+      <div className='link mt1'>distributions</div>
+      <div className='link mt1'>i18n</div>
+      <div className='link mt1'>ipfs-docs</div>
+      <div className='link mt1'>ipfs-gui</div>
+      <div className='link mt1'>js.ipfs.io</div>
+      <div className='link mt1'>public-gateway-checker</div>
+      <div className='link mt1'>website</div>
+
+      <div className='f5 fw4 montserrat charcoal ttu mt3'>ipfs-shipyard org</div>
+      <div className='link mt1'>awesome-ipfs</div>
+      <div className='link mt1'>ipfs-companion</div>
+      <div className='link mt1'>ipfs-css</div>
+      <div className='link mt1'>ipfs-desktop</div>
+      <div className='link mt1'>ipfs-share-files</div>
+      <div className='link mt1'>ipfs-ui-style-guide</div>
+      <div className='link mt1'>ipfs-webui</div>
+      <div className='link mt1'>ipld-explorer</div>
+      <div className='link mt1'>ipld-explorer-components</div>
+    </div>
+  )
+}
+
+export default Nav;
