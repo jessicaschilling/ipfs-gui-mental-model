60d38d88a066c0b9e3d3b713a9d5f040f098a0fe
Jessica Schilling <jessica@protocol.ai>
2020-06-10 00:25:12 -0600

    Blank stakeholder journey maps

--- src/components/StakeholdersCryptoEnthusiastsNonDevs.js
+++ src/components/StakeholdersCryptoEnthusiastsNonDevs.js
@@ -3,7 +3,113 @@ import React from 'react';
 const StakeholdersCryptoEnthusiastsNonDevs = () => (
   <div id='content' className='pt3 pr4 pl4 w-100'>
     <h1><span className='nav-category'>Stakeholders:</span> Cryptocurrency enthusiasts &mdash; non-devs</h1>
-  </div>
-);
+
+        <h2>Simplified stakeholder journey</h2>
+        <div className='chart-box journey-chart w-100'>
+          <div className='flex'>
+            <div className='journey-label'>
+              Stages
+            </div>
+            <div className='journey-box bg-aqua-muted'>
+              Curiosity/info-gathering
+            </div>
+            <div className='journey-box bg-aqua-muted'>
+              Evaluating IPFS
+            </div>
+            <div className='journey-yes'>
+              <span role="img" aria-label="yes">👍</span>
+            </div>
+            <div className='journey-box bg-aqua-muted'>
+              Baseline learning & early building
+            </div>
+            <div className='journey-box bg-aqua-muted'>
+              In-depth building
+            </div>
+            <div className='journey-box bg-aqua-muted'>
+              Problem-solving
+            </div>
+            <div className='journey-box bg-aqua-muted'>
+              Scaling
+            </div>
+          </div>
+          <div className='flex mt3'>
+            <div className='journey-label'>
+              Goals
+            </div>
+            <div className='journey-box bg-white'>
+              Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-yes'>
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+          </div>
+          <div className='flex mt3'>
+            <div className='journey-label'>
+              Pain points
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-yes'>
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+          </div>
+          <div className='flex mt3'>
+            <div className='journey-label'>
+              Repos used
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-yes'>
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+            <div className='journey-box bg-white'>
+            Foo
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
 
 export default StakeholdersCryptoEnthusiastsNonDevs;
