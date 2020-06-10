2dd249f287923f79cb7d47a3aa721746a6f7cc59
Jessica Schilling <jessica@protocol.ai>
2020-06-09 23:33:07 -0600

    Repo charts populated, still missing savvy though

--- src/components/ReposIpfsShipyardIpfsWebui.js
+++ src/components/ReposIpfsShipyardIpfsWebui.js
@@ -6,19 +6,19 @@ const ReposIpfsShipyardIpfsWebui = () => (
     <h1><span className='nav-category'>Repo:</span> ipfs-shipyard/ipfs-webui</h1>
 
     <h2>Core goal relevance</h2>
-    <p>Note: Relevance can be either inherent to the repo itself, or to the repo's ability to facilitate the <em>user's</em> core goals.</p>
+
     <div className='chart-box'>
       <div>
         <dl>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Disaster resilience/recovery</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Corporate/gov't freedom</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Data integrity</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Self-sovereign identity</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>File storage/retrieval</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>File streaming</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Permanent archiving</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Big data</span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'>Developer enablement</span></dd>
+          <dd class='percentage percentage-1'><span class='percentage-label'>Disaster resilience/recovery</span></dd>
+          <dd class='percentage percentage-1'><span class='percentage-label'>Corporate/gov't freedom</span></dd>
+          <dd class='percentage percentage-20'><span class='percentage-label'>Data integrity</span></dd>
+          <dd class='percentage percentage-1'><span class='percentage-label'>Self-sovereign identity</span></dd>
+          <dd class='percentage percentage-100'><span class='percentage-label'>File storage/retrieval</span></dd>
+          <dd class='percentage percentage-20'><span class='percentage-label'>File streaming</span></dd>
+          <dd class='percentage percentage-66'><span class='percentage-label'>Permanent archiving</span></dd>
+          <dd class='percentage percentage-33'><span class='percentage-label'>Big data</span></dd>
+          <dd class='percentage percentage-100'><span class='percentage-label'>Developer enablement</span></dd>
         </dl>
       </div>
     </div>
@@ -28,13 +28,13 @@ const ReposIpfsShipyardIpfsWebui = () => (
     <div className='chart-box'>
       <div>
         <dl>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/devs-dedicated-pro-collab'>Devs: dedicated/pro/collab</NavLink></span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/devs-hobbyist-hacker'>Devs: hobbyist/hacker</NavLink></span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/business-users-engineering-managers'>Business users: eng mgrs</NavLink></span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/crypto-enthusiasts-devs'>Crypto enthusiasts: devs</NavLink></span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/crypto-enthusiats-non-devs'>Crypto enthusiasts: non-devs</NavLink></span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/dweb-curious-relevant-area'>Dweb-curious: relevant area</NavLink></span></dd>
-          <dd class='percentage percentage-0'><span class='percentage-label'><NavLink to='/stakeholders/dweb-curious-civilians'>Dweb-curious: “civilians”</NavLink></span></dd>
+          <dd class='percentage percentage-100'><span class='percentage-label'><NavLink to='/stakeholders/devs-dedicated-pro-collab'>Devs: dedicated/pro/collab</NavLink></span></dd>
+          <dd class='percentage percentage-100'><span class='percentage-label'><NavLink to='/stakeholders/devs-hobbyist-hacker'>Devs: hobbyist/hacker</NavLink></span></dd>
+          <dd class='percentage percentage-25'><span class='percentage-label'><NavLink to='/stakeholders/business-users-engineering-managers'>Business users: eng mgrs</NavLink></span></dd>
+          <dd class='percentage percentage-33'><span class='percentage-label'><NavLink to='/stakeholders/crypto-enthusiasts-devs'>Crypto enthusiasts: devs</NavLink></span></dd>
+          <dd class='percentage percentage-33'><span class='percentage-label'><NavLink to='/stakeholders/crypto-enthusiats-non-devs'>Crypto enthusiasts: non-devs</NavLink></span></dd>
+          <dd class='percentage percentage-66'><span class='percentage-label'><NavLink to='/stakeholders/dweb-curious-relevant-area'>Dweb-curious: relevant area</NavLink></span></dd>
+          <dd class='percentage percentage-66'><span class='percentage-label'><NavLink to='/stakeholders/dweb-curious-civilians'>Dweb-curious: “civilians”</NavLink></span></dd>
         </dl>
       </div>
     </div>
@@ -43,7 +43,7 @@ const ReposIpfsShipyardIpfsWebui = () => (
     <div className='chart-box'>
       <div>
         <dl>
-        <dd class='percentage percentage-0'><span class='percentage-label'>Disaster resilience/recovery</span></dd>
+        <dd class='percentage percentage-0'><span class='percentage-label'>Label</span></dd>
         </dl>
       </div>
     </div>
